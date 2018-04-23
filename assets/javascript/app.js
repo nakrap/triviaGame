503b37d674b3669d7d89b003023d0c06cab25192
Nick Akrap <nickakrap@Nicks-MacBook-Pro.local>
2018-04-23 15:09:46 -0500

    added more stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,8 +17,118 @@ var questions = [{
     correctAnswer: 1
 }];
 
-var currentQuestion = 0;
-var correctAnswers = 0;
+
+var questionContainer = $("#questionArea");
+var score = 0;
 var quizOver = false;
+var clock;
+var timerInterval;
+var timer = $('#timer');
 
 $(document).ready(function () {
+
+  $('#start-btn').on('click', function() {
+    $(this).remove();
+    playGame();
+    $('#reset-btn').hide ();
+  });
+
+  function startTimer() {
+    timer.text(clock);
+
+    timerInterval = setInterval(function() {
+    clock-=1;
+    timer.text(clock);
+
+    if(clock == 0) {
+      timer.empty();
+      scoreGame();
+    }
+
+    }, 1000);
+  }
+
+  function playGame() {
+    clock = 10;
+      questionContainer.empty();
+
+    setUpBoard();
+    makeScoreGameBtn();
+    startTimer();
+  }
+
+  function makeScoreGameBtn() {
+    var scoreGameBtn = $('<button>');
+    scoreGameBtn.text('Submit');
+
+    scoreGameBtn.on('click', scoreGame);
+
+    questionContainer.append(scoreGameBtn);
+
+  }
+
+  function scoreGame() {
+    clearInterval(timerInterval);
+
+    var answers = $('input:checked');
+
+    for(var i = 0; i < questions.length; i++) {
+      var correctAnswer = questions[i].choices[questions[i].correctAnswer];
+
+      if(correctAnswer == $(answers[i]).attr('answer')) {
+        score++;
+      }
+    }
+
+    var scoreResult = $("<p>");
+    scoreResult.text("You got " + score + " questions right.");
+
+    questionContainer.empty();
+
+    questionContainer.append(scoreResult);
+
+    $('#reset-btn').show ();
+    $('#reset-btn').on ('click', playGame);
+    // setTimeout(playGame, 5000);
+
+    
+
+  }
+  
+
+  function setUpBoard() {
+
+    for(var i = 0; i < questions.length; i++) {
+      var questionDiv = $('<div>');
+      questionDiv.attr('question', 'question' + i);
+      var currentQuestion = questions[i];
+      var questionBox = $('<p>');
+      questionBox.text(currentQuestion.question);
+
+      var answersBox = $('<div>');
+
+      for(var j = 0; j < currentQuestion.choices.length; j++) {
+        var currentAnswerChoice = currentQuestion.choices[j];
+        var currentAnswerBox = $('<input>' + currentAnswerChoice + '</input>');
+        currentAnswerBox.attr({type: 'radio',
+                              name: 'answer' + i,
+                              answer: currentAnswerChoice});
+        answersBox.append(currentAnswerBox);
+      }
+
+      questionDiv.append(questionBox);
+      questionDiv.append(answersBox);
+
+      questionContainer.append(questionDiv);
+
+    }
+
+
+
+
+  }
+
+
+
+
+});
